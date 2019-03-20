38b1a18d0062854ec8fdcdaa1a0dc86a9403c71d
Johannes Neumeier <hello@johannesneumeier.com>
2019-03-20 15:49:15 +0200

    sync anchors script has better new anchor positionining honoring original anchor alignment to metrics lines

--- Anchors/sync_anchors_to_other_masters.py
+++ Anchors/sync_anchors_to_other_masters.py
@@ -9,8 +9,10 @@ def syncAnchorsToOtherMasters(layer):
 	anchors = layer.anchors
 	anchorsNames = [a.name for a in anchors]
 	
-	if not anchors:
-		return
+	# also run when syncing "empty" anchors to other masters, e.g. removing
+	# all anchors for all layers; otherwise the following could be retained
+# 	if not anchors:
+# 		return
 	
 	for l in glyph.layers:
 		if l != layer:
@@ -19,8 +21,20 @@ def syncAnchorsToOtherMasters(layer):
 			# add missing anchors in other layers
 			for a in anchors:
 				if a.name not in layeranchors:
-					# TODO position new anchors smartly; either relative to original, or as per set_default_anchors.py
-					l.anchors = [GSAnchor(a.name, NSPoint(a.x, a.y)) for a in anchors]
+					# position the x component of synced anchors at the same percentual
+					# position in the glyph advance width					
+					oldXPercent = a.position.x / layer.width
+					newX = int(l.width * oldXPercent)
+					
+					# when copying an anchor that is on a metrics line in the original
+					# layer try to insert the synced anchor at same metrics line in that layer
+					newY = a.position.y
+					for ypos in ["descender", "xHeight", "capHeight", "ascender"]:
+						if a.position.y == layer.master.__getattribute__(ypos):
+							# print "Set anchor y also to %s" % ypos
+							newY = l.master.__getattribute__(ypos)
+
+					l.anchors = [GSAnchor(a.name, NSPoint(newX, newY)) for a in anchors]
 					
 			# remove anchors in layer that are not in source
 			l.anchors = [a for a in l.anchors if a.name in anchorsNames]
